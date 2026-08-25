aba9e0b5d38df56921b5ae676f14d1e2a26c3571
kavana0406 <pnkavana@gmail.com>
2026-08-25 19:16:41 +0530

    Create functions.py

---
+++ python_basics/functions.py
@@ -0,0 +1,14 @@
+# Python Functions
+
+def greet(name):
+    print("Hello,", name)
+
+
+def add_numbers(a, b):
+    return a + b
+
+
+greet("Kavana")
+
+result = add_numbers(10, 20)
+print("Sum:", result)
