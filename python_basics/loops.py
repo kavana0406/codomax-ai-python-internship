8a915c11c1e5d3611c5bd388426f7baa6f1de15f
kavana0406 <pnkavana@gmail.com>
2026-08-25 19:15:19 +0530

    Create loops.py

---
+++ python_basics/loops.py
@@ -0,0 +1,17 @@
+# Python Loops
+
+print("For loop:")
+for i in range(1, 6):
+    print(i)
+
+print("\nWhile loop:")
+
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
+
+print("\nMultiplication table of 5:")
+
+for i in range(1, 11):
+    print("5 x", i, "=", 5 * i)
