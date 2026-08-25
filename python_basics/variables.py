66fd03d179e122f78b88105cf223b8bc49b41c15
kavana0406 <pnkavana@gmail.com>
2026-08-25 19:11:41 +0530

    Create variables.py

---
+++ python_basics/variables.py
@@ -0,0 +1,11 @@
+# Python Variables and Data Types
+
+name = "Kavana"
+age = 20
+height = 5.4
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
